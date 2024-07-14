c76f1a08464ae8d21e38bc92beca62cfcefbec4d
Wout Schoovaerts <wout@z-soft.be>
2024-07-14 14:00:00 +0100

    06-03 Web Starter: Cleanup index.php

--- ExampleUsage/public/index.php
+++ ExampleUsage/public/index.php
@@ -1,20 +1,7 @@
 <?php
 
-use JMS\Serializer\SerializerBuilder;
-use PhpBoot\Di\Container\ServiceContainer;
-use PhpBoot\Di\Inject\ServiceCreator;
-use PhpBoot\Di\Property\PropertiesReader;
-use PhpBoot\Di\Property\PropertyRegistry;
-use PhpBoot\Di\Scan\ServiceScanner;
-use PhpBoot\Http\Common\HttpStatusCode;
+use App\AppKernel;
 use PhpBoot\Http\Request\RequestFactory;
-use PhpBoot\Http\Response\JsonResponse;
-use PhpBoot\Http\Response\Response;
-use PhpBoot\Http\Routing\Generator\RouteArgumentGenerator;
-use PhpBoot\Http\Routing\Generator\RouteResponseGenerator;
-use PhpBoot\Http\Routing\Matcher\RouteMatcher;
-use PhpBoot\Http\Routing\Router;
-use PhpBoot\Http\Routing\Scan\RouteScanner;
 
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
@@ -25,33 +12,7 @@ ini_set('xdebug.var_display_max_data', 1024);
 
 require dirname(__DIR__) . '/vendor/autoload.php';
 
-$propertyReader = new PropertiesReader();
-$readProperties = $propertyReader->readProperties(dirname(__DIR__) . '/config/properties.yaml');
-$propertyRegistry = new PropertyRegistry($readProperties);
-
-$psr4Mappings = require dirname(__DIR__) . '/vendor/composer/autoload_psr4.php';
-$namespacesToScan = ['App'];
-
-$serviceScanner = new ServiceScanner($psr4Mappings, $namespacesToScan);
-$scannedServices = $serviceScanner->scan();
-
-$serviceCreator = new ServiceCreator($propertyRegistry);
-$beanMap = $serviceCreator->createServices($scannedServices);
-
-$container = new ServiceContainer($beanMap);
-
-$routeScanner = new RouteScanner();
-$routes = $routeScanner->scanForRoutes($container);
-
-$serializer = SerializerBuilder::create()->build();
-$router = new Router(
-    new RouteMatcher($routes),
-    new RouteArgumentGenerator($serializer),
-    new RouteResponseGenerator($serializer)
-);
-
 $request = RequestFactory::createFromGlobals();
-$response = $router->createResponseFromRequest($request);
+$kernel = new AppKernel();
 
-$response->prepare($request);
-$response->send();
+$kernel->handleRequest($request);
---
+++ ExampleUsage/src/AppKernel.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App;
+
+use PhpBoot\Starter\Web\WebKernel;
+
+class AppKernel extends WebKernel
+{
+
+    #[\Override] protected function getNamespacesToScan(): array
+    {
+        return [
+            'App',
+            'PhpBoot\\Starter\\Web\\'
+        ];
+    }
+
+    #[\Override] protected function getPsr4Mappings(): array
+    {
+        return require dirname(__DIR__) . '/vendor/composer/autoload_psr4.php';
+    }
+
+    #[\Override] protected function getPropertiesFilePath(): string
+    {
+        return dirname(__DIR__) . '/config/properties.yaml';
+    }
+
+    #[\Override] protected function getRootDirectory(): string
+    {
+        return dirname(__DIR__);
+    }
+
+    #[\Override] protected function getCacheDirectory(): string
+    {
+        return dirname(__DIR__) . '/cache';
+    }
+}
