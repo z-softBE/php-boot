bbb27554d546f1fb5943fada48ceafd3b527f3c5
Wout Schoovaerts <wout@z-soft.be>
2024-07-14 14:00:00 +0100

    03-07 DI: Testing Property Registry & reader

--- ExampleUsage/public/index.php
+++ ExampleUsage/public/index.php
@@ -1,5 +1,7 @@
 <?php
 
+use PhpBoot\Di\Property\PropertiesReader;
+use PhpBoot\Di\Property\PropertyRegistry;
 use PhpBoot\Di\Scan\ServiceScanner;
 
 error_reporting(E_ALL);
@@ -10,6 +12,15 @@ ini_set('xdebug.var_display_max_children', 256);
 ini_set('xdebug.var_display_max_data', 1024);
 
 require dirname(__DIR__) . '/vendor/autoload.php';
+
+$propertyReader = new PropertiesReader();
+$readProperties = $propertyReader->readProperties(dirname(__DIR__) . '/config/properties.yaml');
+$propertyRegistry = new PropertyRegistry($readProperties);
+
+var_dump($propertyRegistry);
+var_dump($propertyRegistry->getPropertyByName('hello.world'));
+var_dump($propertyRegistry->getPropertyByNameOrDefault('hello.world2', 'default'));
+die;
 
 $psr4Mappings = require dirname(__DIR__) . '/vendor/composer/autoload_psr4.php';
 $namespacesToScan = ['App'];
